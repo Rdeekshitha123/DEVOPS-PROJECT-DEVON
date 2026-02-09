abeb792e1a1e5c11f9aea5a97f2125b31c295711
Deekshitha Ramalingegowda (DevOn) <d.ramalingegowda@devon.nl>
2026-02-09 12:28:27 +0530

    change9

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,10 +5,3 @@ output "ec2_instance_ip" {
   value = module.ec2.instance_public_ip
 }
 
-output "application_url" {
-  value = "http://${module.lb.alb_dns_name}"
-}
-
-output "direct_access_url" {
-  value = "http://${module.ec2.instance_public_ip}"
-}
