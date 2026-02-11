1ba21d418bb0b0a7069e7ae32a4cc3bb9aacac0c
Deekshitha Ramalingegowda (DevOn) <d.ramalingegowda@devon.nl>
2026-02-11 17:50:07 +0530

    change44

--- terraform/modules/ASG/main.tf
+++ terraform/modules/ASG/main.tf
@@ -43,8 +43,8 @@ resource "aws_autoscaling_group" "my_asg" {
   name = "my-asg"
 
   min_size         = 1
-  max_size         = 3
-  desired_capacity = 1
+  max_size         = 5
+  desired_capacity = 2
 
   vpc_zone_identifier = [var.subnet_id]
 
