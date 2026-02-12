593840cb24add0a8e6ad1bfa33c22fc929e202ad
Deekshitha Ramalingegowda (DevOn) <d.ramalingegowda@devon.nl>
2026-02-12 11:38:21 +0530

    change48

--- terraform/modules/ASG/main.tf
+++ terraform/modules/ASG/main.tf
@@ -53,7 +53,7 @@ resource "aws_autoscaling_group" "my_asg" {
     version = "$Latest"
   }
 
-  health_check_type         = "ELB"
+  health_check_type         = "EC2"
   health_check_grace_period = 300
 
   tag {
