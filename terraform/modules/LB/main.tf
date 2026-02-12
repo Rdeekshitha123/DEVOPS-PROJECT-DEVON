1ab55189ac1331aa4881ab29973cb4568ca2efe0
Deekshitha Ramalingegowda (DevOn) <d.ramalingegowda@devon.nl>
2026-02-12 21:53:21 +0530

    change75

--- terraform/modules/LB/main.tf
+++ terraform/modules/LB/main.tf
@@ -20,7 +20,6 @@ resource "aws_lb_target_group" "my_tg" {
         protocol = "HTTP"
         path = "/"
         matcher = "200"
-        interval = 30
         timeout = 5
         healthy_threshold = 2
         unhealthy_threshold = 2 
